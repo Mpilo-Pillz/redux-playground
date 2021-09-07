4e176fcb14ae01ae7b506e23b182d904e5e7cfec
Fikasentani <mpilo.dlamini@yahoo.com>
2021-09-07 05:21:31 +0200

    initial commit of redux basics

---
+++ redux-demo.js
@@ -0,0 +1,19 @@
+const redux = require('redux');
+
+//** reducer functions take 2 inputs old state plus dispatched actions */
+// returns new state pure function no side effects - dont get stuff from local storage or send http request or fetch something from local storage
+const counterReducer = (state = { counter: 0 }, action) => {
+    return {
+        counter: state.counter + 1
+    }
+};
+const store = redux.createStore(counterReducer); //point at the function dont execute the reducer will do that
+// console.log(store.getState());
+const counterSubscriber = () => {
+    const latestState = store.getState();
+    console.log(latestState);
+}
+
+store.subscribe(counterSubscriber); //point at the function dont execute the reducer will do that
+
+store.dispatch({ type: 'increment' }) // whatchout it will print 2 not one cos i return new state
