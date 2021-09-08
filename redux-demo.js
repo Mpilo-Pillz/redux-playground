58b4b41934e791ef0639a88320bd53644238f859
Fikasentani <mpilo.dlamini@yahoo.com>
2021-09-08 12:37:49 +0200

    adding decrement action

--- redux-demo.js
+++ redux-demo.js
@@ -3,8 +3,16 @@ const redux = require('redux');
 //** reducer functions take 2 inputs old state plus dispatched actions */
 // returns new state pure function no side effects - dont get stuff from local storage or send http request or fetch something from local storage
 const counterReducer = (state = { counter: 0 }, action) => {
-    return {
-        counter: state.counter + 1
+    if (action.type === 'increment') {
+        return {
+            counter: state.counter + 1
+        }
+    }
+
+    if (action.type === 'decrement') {
+        return {
+            counter: state.counter - 1
+        }
     }
 };
 const store = redux.createStore(counterReducer); //point at the function dont execute the reducer will do that
@@ -17,3 +25,4 @@ const counterSubscriber = () => {
 store.subscribe(counterSubscriber); //point at the function dont execute the reducer will do that
 
 store.dispatch({ type: 'increment' }) // whatchout it will print 2 not one cos i return new state
+store.dispatch({ type: 'decrement' })
