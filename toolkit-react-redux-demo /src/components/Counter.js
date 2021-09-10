a9a9740660f96f7b5ac697aa6338f734299215ff
Fikasentani <mpilo.dlamini@yahoo.com>
2021-09-10 18:44:15 +0200

    creating counter version with redux toolkit

---
+++ toolkit-react-redux-demo /src/components/Counter.js
@@ -0,0 +1,46 @@
+import { useDispatch, useSelector } from 'react-redux'; // manage a part of our state managed by the store
+// import { useStore } from 'react-redux'; // utap directly into our store
+import classes from './Counter.module.css';
+import { counterActions } from '../store';
+
+const Counter = () => {
+  //select which data we want to select from store
+  const counter = useSelector(state => state.counter);
+  const show = useSelector(state => state.showCounter)
+  const dispatch = useDispatch();
+  const toggleCounterHandler = () => {
+    // dispatch({ type: 'toggle' })
+    dispatch(counterActions.toggleCounter())
+  };
+
+  const incrementHandler = () => {
+    // dispatch({ type: 'increment', amount: 5 })
+
+    dispatch(counterActions.increment())
+  };
+
+  const increaseHandler = () => {
+    // dispatch({ type: 'increase', amount: 5 })
+    // reducer: {counter: counterSlice.reducer}
+    dispatch(counterActions.increase(5)) // automatically creates a payload property for 10
+  };
+
+  const decrementHandler = () => {
+    // dispatch({ type: 'decrement' })
+    dispatch(counterActions.decrement())
+  }
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Functional Counter</h1>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increment by 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
