814f38a9b4df53052c06daf7b961e1d4d1afe842
Fikasentani <mpilo.dlamini@yahoo.com>
2021-09-12 19:18:00 +0200

    witing controlss and ui to state

--- async-react-redux/src/components/Shop/Products.js
+++ async-react-redux/src/components/Shop/Products.js
@@ -1,16 +1,35 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    price: 10,
+    title: 'Book title',
+    description: "Book description"
+  },
+  {
+    id: 'p2',
+    price: 20,
+    title: 'Second Book title',
+    description: "SEcond Book description"
+  }
+]
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
+
       </ul>
     </section>
   );
