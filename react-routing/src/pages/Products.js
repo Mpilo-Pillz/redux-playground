f5a3d8c3ae824ef1bcb638e5e478cce13916f9f2
Fikasentani <mpilo.dlamini@yahoo.com>
2021-09-14 05:07:27 +0200

    adding router switch to handl nested routes

--- react-routing/src/pages/Products.js
+++ react-routing/src/pages/Products.js
@@ -1,11 +1,12 @@
+import { Link } from 'react-router-dom';
 const Products = () => {
     return (
         <section>
             <h1>Products Page</h1>
             <ul>
-                <li>PRod1</li>
-                <li>PRod2</li>
-                <li>PRod3</li>
+                <li><Link to='/products/p1'>Ergronomic Chair</Link></li>
+                <li><Link to='/products/p2'>Bluetooth Keyboard and mouse</Link></li>
+                <li><Link to='/products/p3'>Air pods</Link></li>
             </ul>
         </section>
     )
