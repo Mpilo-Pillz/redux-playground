16d274cb44729c71e04fb1d53889d35876167ddc
Fikasentani <mpilo.dlamini@yahoo.com>
2021-09-14 04:28:17 +0200

    adding react router link

--- react-routing/src/App.js
+++ react-routing/src/App.js
@@ -1,9 +1,11 @@
 import { Route } from 'react-router-dom';
 import Welcome from './pages/Welcome';
-import Products from './pages/Products'
+import Products from './pages/Products';
+import MainHeader from './components/Main-Header';
 function App() {
   return (
     <div>
+      <MainHeader />
       <Route path="/welcome">
         <Welcome />
       </Route>
