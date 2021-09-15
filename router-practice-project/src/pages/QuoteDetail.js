298a465f7893c030068061804de24e6813046404
Fikasentani <mpilo.dlamini@yahoo.com>
2021-09-15 05:00:02 +0200

    adding nested routes

--- router-practice-project/src/pages/QuoteDetail.js
+++ router-practice-project/src/pages/QuoteDetail.js
@@ -1,10 +1,13 @@
-import { useParams } from 'react-router-dom';
-
+import { useParams, Route } from 'react-router-dom';
+import Comments from '../components/comments/Comments';
 const QuoteDetail = () => {
     const params = useParams()
     return (
         <>
             <h1>{params.quoteId}</h1>
+            <Route path={`/quotes/${params.quoteId}/comments`}>
+                <Comments />
+            </Route>
         </>
     )
 }
