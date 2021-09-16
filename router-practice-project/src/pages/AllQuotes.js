1107ea66407941734191be4e7c27edea6e0d7138
Fikasentani <mpilo.dlamini@yahoo.com>
2021-09-16 04:40:08 +0200

    adding quote detail and quite form

--- router-practice-project/src/pages/AllQuotes.js
+++ router-practice-project/src/pages/AllQuotes.js
@@ -1,5 +1,10 @@
+import QuoteList from '../components/quotes/QuoteList';
+const DUMMY_QUOTES = [
+    { id: 'q1', author: 'Mapee', text: 'Learning React is fun' },
+    { id: 'q2', author: 'Heleb', text: 'Mastering fundermentals pays dividends.' },
+]
 const AllQuotes = () => {
-    return <h1>p</h1>
+    return <QuoteList quotes={DUMMY_QUOTES} />
 }
 
 export default AllQuotes;
