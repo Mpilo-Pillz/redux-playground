f5c68aee9dc3c5f105d651a76a71db5bcf61aa2e
Fikasentani <mpilo.dlamini@yahoo.com>
2021-09-17 04:34:58 +0200

    adding quite deatail

--- router-practice-project/src/pages/QuoteDetail.js
+++ router-practice-project/src/pages/QuoteDetail.js
@@ -1,10 +1,22 @@
 import { useParams, Route } from 'react-router-dom';
 import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+
+const DUMMY_QUOTES = [
+    { id: 'q1', author: 'Mapee', text: 'Learning React is fun' },
+    { id: 'q2', author: 'Heleb', text: 'Mastering fundermentals pays dividends.' },
+]
+
 const QuoteDetail = () => {
-    const params = useParams()
+    const params = useParams();
+    const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId)
+
+    if (!quote) {
+        return <p>No quote found</p>
+    }
     return (
         <>
-            <h1>{params.quoteId}</h1>
+            <HighlightedQuote text={quote.text} author={quote.author} />
             <Route path={`/quotes/${params.quoteId}/comments`}>
                 <Comments />
             </Route>
