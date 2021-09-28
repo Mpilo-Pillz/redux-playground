1a70c02ecb6393a76f137e524c1762a5885e5213
Fikasentani <mpilo.dlamini@yahoo.com>
2021-09-28 05:54:09 +0200

    adding uselogger custom hook

--- fundermentals-react/src/components/CustomHooks.jsx
+++ fundermentals-react/src/components/CustomHooks.jsx
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
 import useLocalStorage from './useLocalStorage';
+import useUpdateLogger from './useUpdateLogger';
 
 const CustomHook = () => {
     // const [name, setName] = useState('');
     // const [name, setName] = useLocalStorage('name', '');
     const [name, setName] = useLocalStorage('name', () => '');
-
+    useUpdateLogger(name)
     return (
+        <>
+        <p>open devtools lok at console and aplication local storage</p>
         <input type="text"
         value={name}
         onChange={e => setName(e.target.value)}
         />
+        </>
     )
 }
 
