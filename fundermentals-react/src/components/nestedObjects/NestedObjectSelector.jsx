7f96f0c3bb93260f580e135a2ee2b2fb8c77a091
Fikasentani <mpilo.dlamini@yahoo.com>
2021-10-02 11:04:30 +0200

    making site trnalsatable with boject keys

--- fundermentals-react/src/components/nestedObjects/NestedObjectSelector.jsx
+++ fundermentals-react/src/components/nestedObjects/NestedObjectSelector.jsx
@@ -1,19 +1,36 @@
+import { useState } from "react";
+
 const DATA_OBJECT = {
     english: {
         date: "Date",
         keys: "Keys",
-        ready: "Ready"
+        ready: "Ready",
+        orange: "orange"
     },
     french: {
         date: "Rendez-vouz",
         keys: "Cles",
-        ready: "Pret"
+        ready: "Pret",
+        orange: "narange"
+    },
+    spanish: {
+        date: "Rendez-vouz",
+        keys: "Cles",
+        ready: "Pret",
+        orange: "orange"
     }
 }
 
 const SelectTranslation = () => {
+    const [language, setLanguage] = useState({
+        date: "Date",
+        keys: "Keys",
+        ready: "Ready",
+        orange: "orange"
+    })
     return (
-        <select name="" id="">
+        <>
+        <select name="" id="" onChange={(e) => setLanguage(DATA_OBJECT[e.target.value])}>
         {Object.keys(DATA_OBJECT).map((word, i) => {
             console.log("woorrdd-->",word);
             return (
@@ -21,6 +38,8 @@ const SelectTranslation = () => {
             )
         })}
         </select>
+        {language.date}
+        </>
     )
 }
 
