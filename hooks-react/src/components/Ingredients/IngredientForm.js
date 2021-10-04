ff71f76b906e861b7a4cb3753c1d85aaeef05347
Fikasentani <mpilo.dlamini@yahoo.com>
2021-10-04 04:18:01 +0200

    renamong state values

--- hooks-react/src/components/Ingredients/IngredientForm.js
+++ hooks-react/src/components/Ingredients/IngredientForm.js
@@ -4,8 +4,8 @@ import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
-  const [title, setTitle] = useState('');
-  const [amount, setAmount] = useState(amount);
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
 
   const submitHandler = event => {
     event.preventDefault();
@@ -18,11 +18,11 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" value={title} onChange={event => setTitle(event.target.value)} />
+            <input type="text" id="title" value={enteredTitle} onChange={event => setEnteredTitle(event.target.value)} />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" value={amount} />
+            <input type="number" id="amount" value={enteredAmount} onChange={event => setEnteredAmount(event.target.value)} />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
