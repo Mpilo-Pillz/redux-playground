50f8e2fc2f622c33242bf4a1ea6d3296e2834ed0
Fikasentani <mpilo.dlamini@yahoo.com>
2021-10-31 11:22:38 +0200

    adding styles

--- vite-react-ts/src/components/Todos.tsx
+++ vite-react-ts/src/components/Todos.tsx
@@ -1,10 +1,11 @@
 import { FC } from "react";
 import { Todo } from "../models/todos";
 import { TodoItem } from "./TodoItem";
+import classes from "./Todos.module.css";
 
 const Todos: FC<{ items: Todo[] }> = (props) => {
   return (
-    <ul>
+    <ul className={classes.todos}>
       {props.items.map((item) => (
         <TodoItem key={item.id} text={item.text} />
       ))}
