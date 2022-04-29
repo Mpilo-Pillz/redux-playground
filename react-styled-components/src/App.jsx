d135ce804efddf3104e2aacbf893a7af2a2cc59b
Fikasentani <mpilo.dlamini@yahoo.com>
2022-04-29 18:38:56 +0200

    creating styled container

--- react-styled-components/src/App.jsx
+++ react-styled-components/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
 import { Button } from "./styledComponents/Button.style";
+import { AppContainer } from "./styledComponents/Container.style";
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
+    <AppContainer >
       <Button backgroundColor="red">Click me</Button>
       <Button backgroundColor="green">Click me</Button>
       <Button backgroundColor="blue">Click me</Button>
-    </div>
+    </AppContainer>
   )
 }
 
